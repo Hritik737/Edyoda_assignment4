9f92030dc512ff94a14b16a9bbe750516222a1ee
Hritik737 <105117003+Hritik737@users.noreply.github.com>
2022-06-04 23:44:49 +0530

    Add files via upload

---
+++ find_the_way_with_maps.py
@@ -0,0 +1,4 @@
+# Write a Python program to triple all numbers of a given list of integers. Use Python map.
+list1=[1,2,3,4,5,6,7]
+data=list(map(lambda x:x*3,list1))
+print(data)
---
+++ play_with_lambda.py
@@ -0,0 +1,5 @@
+# Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument.
+given_number=10
+a=[]
+data=lambda a,b:a+b
+print(data(given_number,25))
